57ff206bfa3238e6a3ae964846288cca39916b59
cab11918 <yu.mingh@husky.neu.edu>
2019-10-06 12:20:33 -0400

    implemented features for lesson tabs

--- src/components/ModuleList.js
+++ src/components/ModuleList.js
@@ -46,9 +46,6 @@ const ModuleList = () =>
           </button>
 
 
-
-
-
         </a>
 
         <a className="list-group-item list-group-item-action wbdv-module-item"
@@ -76,9 +73,6 @@ const ModuleList = () =>
             <FontAwesomeIcon icon={faEdit}/>
             &nbsp;
           </button>
-
-
-
 
 
         </a>
@@ -110,13 +104,11 @@ const ModuleList = () =>
           </button>
 
 
-
-
-
-        </a>        <a className="list-group-item list-group-item-action wbdv-module-item"
-                       id="list-module1"
-                       data-toggle="list"
-                       href="#module1" role="tab" aria-controls="module1">
+        </a> <a
+          className="list-group-item list-group-item-action wbdv-module-item"
+          id="list-module1"
+          data-toggle="list"
+          href="#module1" role="tab" aria-controls="module1">
                     <span className="wbdv-module-item-title">
                         Module4-JQuery
                     </span>
@@ -140,13 +132,11 @@ const ModuleList = () =>
         </button>
 
 
-
-
-
-      </a>        <a className="list-group-item list-group-item-action wbdv-module-item"
-                     id="list-module1"
-                     data-toggle="list"
-                     href="#module1" role="tab" aria-controls="module1">
+      </a> <a
+          className="list-group-item list-group-item-action wbdv-module-item"
+          id="list-module1"
+          data-toggle="list"
+          href="#module1" role="tab" aria-controls="module1">
                     <span className="wbdv-module-item-title">
                         Module5-JQuery
                     </span>
@@ -170,13 +160,11 @@ const ModuleList = () =>
         </button>
 
 
-
-
-
-      </a>        <a className="list-group-item list-group-item-action wbdv-module-item"
-                     id="list-module1"
-                     data-toggle="list"
-                     href="#module1" role="tab" aria-controls="module1">
+      </a> <a
+          className="list-group-item list-group-item-action wbdv-module-item"
+          id="list-module1"
+          data-toggle="list"
+          href="#module1" role="tab" aria-controls="module1">
                     <span className="wbdv-module-item-title">
                         Module6-JQuery
                     </span>
@@ -200,25 +188,15 @@ const ModuleList = () =>
         </button>
 
 
-
-
-
       </a>
 
 
-
-
-
-    </div>
+      </div>
       <button
           className="btn btn-success btn-block mt-3 wbdv-module-item-add-btn"
           onClick="window.location.href = '';">
         <i className="fas fa-plus-circle"></i> New Module
       </button>
     </div>
-
-
-
-
 
 export default ModuleList
--- src/containers/CourseEditor.js
+++ src/containers/CourseEditor.js
@@ -10,6 +10,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import CourseTable from "./CourseTable";
 import ModuleList from "../components/ModuleList";
+import LessonTabs from "../components/LessonTabs";
 
 
 const CourseEditor = () =>
@@ -20,68 +21,7 @@ const CourseEditor = () =>
 
 
 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-
-          <Link to="/courseTable">
-          <button className={"btn btn-outline-light float-right"}>
-
-            <FontAwesomeIcon icon={faTimesCircle}/>
-
-          </button>
-
-          </Link>
-          <a className="navbar-brand" href="#">CS5550 Software Engineering Graduate
-          </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse"
-                  data-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent" aria-expanded="false"
-                  aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#">Build <span
-                    className="sr-only">(current)</span></a>
-              </li> <li className="nav-item">
-              <a className="nav-link" href="#">Pages <span
-                  className="sr-only">(current)</span></a>
-            </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Theme <span
-                    className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Store <span
-                    className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Apps <span
-                    className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Settings <span
-                    className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-
-
-                <span
-                    className="sr-only">(current)</span></a>
-              </li>
-
-
-            </ul>
-
-          </div>
-          <button className={"btn btn-outline-light float-right"}>
-
-            <FontAwesomeIcon icon={faPlusSquare}/>
-
-          </button>
-        </nav>
+        <LessonTabs/>
 
         <div className="row">
             <ModuleList/>
