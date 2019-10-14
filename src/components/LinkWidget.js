5c64aa7d426ab875e0a189d43cd3819e94315c83
cab11918 <yu.mingh@husky.neu.edu>
2019-10-14 18:49:56 -0400

    implemented link widget

--- src/components/LinkWidget.js
+++ src/components/LinkWidget.js
@@ -2,7 +2,45 @@ import React from 'react'
 
 const LinkWidget = ({widget}) =>
     <div>
-      Link widget
+
+
+      <label className="h4">List widget</label>
+
+
+
+      <form>
+        <div className="form-group">
+          <input className="form-control"
+
+                 placeholder={widget.href}/>
+
+        </div>
+
+        <div className="form-group">
+          <input className="form-control"
+
+                 placeholder="Link text"/>
+
+        </div>
+
+
+        <div className="form-group">
+          <input className="form-control"
+
+                 placeholder="Widget name"/>
+
+        </div>
+
+      </form>
+
+      <h3>Preview</h3>
+
+      <a href={widget.href}>
+        Link text
+
+      </a>
+
+
     </div>
 
 export default LinkWidget
